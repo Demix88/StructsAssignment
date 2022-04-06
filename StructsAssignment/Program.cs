310adf938e3a9508e3ed82ad58c6f75477d9d2e0
Demix88 <skrasnauskis@gmail.com>
2022-04-06 11:00:46 +0100

    Add project files.

---
+++ StructsAssignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructsAssignment
+{
+    class Program
+    {
+        struct Number
+        {
+            public decimal Amount;
+        }
+        static void Main(string[] args)
+        {
+            Number num1;
+            num1.Amount = 1.47535m;
+            Console.WriteLine("The Decimal Number is:" + "\n" + num1.Amount);
+            Console.ReadLine();
+        }
+    }
+}
